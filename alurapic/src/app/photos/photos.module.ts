041dd1a0bf57c1c8d80cc9da3b000aa00846546a
c1294103 <lcsantos1@stefanini.com>
2019-08-19 14:58:31 -0300

    organizando os codigos para photo ficar independente e app.component ficar somente para renderizar a aplicação

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,12 +1,27 @@
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoService } from './Photo/photo.service';
 
 @NgModule({
-  declarations: [PhotoComponent],
-  exports: [PhotoComponent],
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent],
   imports: [HttpClientModule]
 })
 export class PhotosModule {
+  photos: any[] = [];
 
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit(): void {
+    this.photoService.listFromUser('flavio')
+      .subscribe(
+        photos => {
+          this.photos = photos
+        }
+      );
+  }
 }
