c249e4579b04929add40ebdbd01c51be5def2e2b
c1294103 <lcsantos1@stefanini.com>
2019-08-21 13:52:59 -0300

    criando um componente com modulo e colocando em uma rota errors

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 // Configura as rotas de minha aplicação
 const routes: Routes = [
   { path: 'user/flavio', component: PhotoListComponent},
   { path: 'p/add', component: PhotoFormComponent},
+
+  // roteamento em caso de não existir a página
+  { path: '**', component: NotFoundComponent},
+
+
 ];
 
 @NgModule({
